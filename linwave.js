d8ca1f289b1d9e5c87654002a3e8ff469bbea51e
Nat Wilson <njwilson23@gmail.com>
2014-03-10 11:37:16 -0400

    switch to canvas-based animation

--- linwave.js
+++ linwave.js
@@ -1,80 +1,95 @@
 
-function Waves(k, om, h, eta) {
-  this.k = k;
-  this.om = om;
-  this.eta = eta;
-  this.h = h;
+window.requestAnimationFrame = window.requestAnimationFrame ||
+                              window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame ||
+                              window.msRequestAnimationFrame;
 
-  this.g = 9.8;
-  this.x = Array.apply(null, Array(100)).map(function (_, i) {return i;});
-  return this;
+var k,
+    om,
+    eta,
+    h;
+
+var g = 9.8,
+    n = 100,
+    x = Array.apply(null, Array(n)).map(function (_, i) {return 4*Math.PI/n*i;});
+
+var canvas = document.getElementById('waves');
+var context = canvas.getContext('2d'),
+    height = canvas.height,
+    width = canvas.width;
+
+var startTime = null;
+
+function drawLoop(t) {
+  t = t || new Date.time();
+  if (startTime === null) startTime = t;
+  var elapsedTime = t - startTime;
+  clearFrame();
+  drawFrame(elapsedTime);
+  requestAnimationFrame(drawLoop);
 }
 
-Waves.prototype.draw = function(t) {
-  this.svg = d3.select("div#waveSim")
-    .append("svg")
-      .attr("width", "100%")
-      .attr("height", "80%")
-      .attr("id", "waveSvg");
-
-  var s = this.surface(0);
-  var surface = this.svg.selectAll("circles")
-    .data(s)
-    .enter()
-      .append("circle");
-
-  /*var sfcLine = d3.svg.line()
-    .x(function (d, i) { return 10 + 6*i; })
-    .y(function (d, i) { return 100 + 20*d; })
-    .interpolate("linear");
-
-  this.svg.append("svg:path")
-    .attr("d", sfcLine(this.surface(0)));*/
-
-  this.elements = {};
-  this.elements["surface"] = surface;
-
-  //this.update(0);
-  var i = 0;
-  var w = this;
-  setInterval(function () {
-
-    i++;
-    w.update(2*i);
-
-    //w.elements.surface
-    //  .data(w.surface(t))
-    //  .enter()
-    //    .append("circle");
-
-    //w.elements.surface
-    //  .attr("cx", function (d, i) { return 15+i*6; })
-    //  .attr("cy", function (d, i) { return 100 + 20*d; })
-    //  .attr("r", function (_,_) {return 3; });
-    }, 30);
-
-
-  return this;
+function clearFrame() {
+  context.clearRect(0, 0, width, height);
 }
 
-Waves.prototype.surface = function(t) {
-  // Return the wave surface
-  var u = this.x.map(function (x) {return this.eta * Math.cos(this.k*x - this.om*t);})
-  return u;
+function drawFrame(t) {
+  var h = height,
+      w = width,
+      hts = height / 5;
+
+  // water
+  context.fillStyle = "#8888FF";
+  context.beginPath();
+  var u = waveSurface(t);
+  context.moveTo(w, 0.2*height + 0.2*height*u[-1]);
+  context.lineTo(w, height);
+  context.lineTo(0, height);
+  context.lineTo(0, 0.2*height);
+  for (var i=0; i < u.length; i++) {
+    zs = 0.2 * height + (height-hts) * u[i] / H;
+    context.lineTo(i*w/u.length, zs);
+  }
+  //context.stroke();
+  context.fill();
+  context.closePath();
+
+  // particle
+  addParticle(0.5*w, 0.2*height, t);
+  addParticle(0.5*w, 0.3*height, t);
+  addParticle(0.5*w, 0.5*height, t);
+  addParticle(0.5*w, 0.7*height, t);
+  addParticle(0.5*w, 0.9*height, t);
+
+
 }
 
-Waves.prototype.update = function(t) {
+function addParticle(xs, zs, t) {
+  var hts = height / 5; // pixel location of water surface
+  var z = -((zs - hts) / (height - hts)) * H;
+  var x = xs / width * L;
+  var theta = k*x - om/1000*t;
+  var psi = -eta * Math.sin(theta) * Math.cosh(k*(z+H)) / Math.sinh(k*H);
+  var zeta = eta * Math.cos(theta) * Math.sinh(k*(z+H)) / Math.sinh(k*H);
 
-  this.elements.surface
-    .data(this.surface(t))
-    .enter()
-      .append("circle");
+  var psis = psi * width / L,
+      zetas = zeta * (height-hts) / H;
 
-  this.elements.surface
-    .attr("cx", function (d, i) { return 15+i*6; })
-    .attr("cy", function (d, i) { return 100 + 20*d; })
-    .attr("r", function (_,_) {return 3; });
+  context.fillStyle = "#000000";
+  context.beginPath();
+  context.arc(xs + psis, zs + zetas, 3, 0, 2*Math.PI);
+  context.stroke();
+  context.fill();
+  context.closePath();
+
   return;
 }
 
+function waveSurface(t) {
+  var u = x.map(function (x) {
+      var xm = x / width * L;
+      return eta * Math.cos(k*x - om/1000*t);
+    });
+  return u;
+}
 
